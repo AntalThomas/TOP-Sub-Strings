3b0cdb96fed8bc4f85dc4756334ca6a9b292c2b4
AntalThomas <Blake-Heward@hotmail.com>
2022-09-17 18:27:12 +0930

    Completed Project

---
+++ sub-strings.rb
@@ -0,0 +1,19 @@
+def subString(string, dictionary)
+    wordArray = Hash.new(0)
+    cleanString = string.downcase.gsub(/[^a-z0-9\s]/i, '').split(" ")
+
+
+    for word in dictionary do
+        for subStr in cleanString
+            if subStr.scan(/#{word}/).count > 0
+                wordArray[word] += 1
+            end
+        end
+    end
+
+    return wordArray
+end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+puts subString("Howdy partner, sit down! How's it going?", dictionary)
